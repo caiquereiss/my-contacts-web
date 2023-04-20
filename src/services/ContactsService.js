1d3f59170a6eb7aa276775cfe45382d0a0d56e68
Caique Reis <caiquereiss@outlook.com>
2023-04-19 21:17:13 -0300

    separating responsibility

--- src/services/ContactsService.js
+++ src/services/ContactsService.js
@@ -1,9 +1,18 @@
 import HttpClient from './utils/HttpClient';
 
 class ContactsService {
+  constructor() {
+    this.httpClient = new HttpClient('http://localhost:3001');
+  }
+
   async listContacts(orderBy = 'asc') {
-    return HttpClient.get(`http://localhost:3001/contacts?orderBy=${orderBy}`);
+    return this.httpClient.get(`/contacts?orderBy=${orderBy}`);
+  }
+
+  async createContact(contact) {
+    return this.httpClient.get('/contacts', contact);
   }
 }
 
 export default new ContactsService();
+// http://localhost:3001
--- src/services/utils/HttpClient.js
+++ src/services/utils/HttpClient.js
@@ -1,11 +1,15 @@
 import delay from '../../utils/delay';
 
 class HttpClient {
-  async get(url) {
-    const response = await fetch(url);
+  constructor(baseURL) {
+    this.baseURL = baseURL;
+  }
+
+  async get(path) {
+    const response = await fetch(`${this.baseURL}${path}`);
     await delay(500);
 
     return response.json();
   }
 }
-export default new HttpClient();
+export default HttpClient;
