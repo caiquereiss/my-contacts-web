cdae51efbfab2574f4b668639e0617aa4c4948f5
Caique Reis <caiquereiss@outlook.com>
2023-04-26 21:16:38 -0300

    parse error

--- src/services/utils/HttpClient.js
+++ src/services/utils/HttpClient.js
@@ -6,10 +6,21 @@ class HttpClient {
   }
 
   async get(path) {
-    const response = await fetch(`${this.baseURL}${path}`);
     await delay(500);
+    const response = await fetch(`${this.baseURL}${path}`);
 
-    return response.json();
+    let body = null;
+    const contentType = response.headers.get('Content-Type');
+    if (contentType.includes('application/json')) {
+      body = await response.json();
+    }
+
+    if (response.ok) {
+      return body;
+    }
+    throw new Error(
+      body?.error || `${response.status} - ${response.statusText}`,
+    );
   }
 }
 export default HttpClient;
