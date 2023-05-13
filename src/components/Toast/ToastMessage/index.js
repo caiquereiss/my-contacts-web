5a67b0165f1ee1d1d37a6f59bddde52f21d995a3
Caique Reis <caiquereiss@outlook.com>
2023-05-13 14:33:35 -0300

    implement remove toast

--- src/components/Toast/ToastMessage/index.js
+++ src/components/Toast/ToastMessage/index.js
@@ -1,24 +1,46 @@
 import PropTypes from 'prop-types';
+import { useEffect } from 'react';
 import { Container } from './styles';
 import xCircleIcon from '../../../assets/images/icons/x-circle.svg';
 import checkIconCircle from '../../../assets/images/icons/check-circle.svg';
 
-export function ToastMessage({ text, type }) {
-  return (
-    <Container type={type}>
-      {type === 'danger' && <img src={xCircleIcon} alt="X" />}
-      {type === 'success' && <img src={checkIconCircle} alt="Check" />}
+export function ToastMessage({
+  message, onRemoveMessage,
+}) {
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      onRemoveMessage(message.id);
+    }, message.duration || 7000);
 
-      <strong>{text}</strong>
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, [message, onRemoveMessage]);
+
+  function handleRemoveToast() {
+    onRemoveMessage(message.id);
+  }
+  return (
+    <Container
+      type={message.type}
+      onClick={handleRemoveToast}
+      tabIndex={0}
+      role="button"
+    >
+      {message.type === 'danger' && <img src={xCircleIcon} alt="X" />}
+      {message.type === 'success' && <img src={checkIconCircle} alt="Check" />}
+
+      <strong>{message.text}</strong>
     </Container>
   );
 }
 
 ToastMessage.propTypes = {
-  text: PropTypes.string.isRequired,
-  type: PropTypes.oneOf(['default', 'success', 'danger']),
-};
-
-ToastMessage.defaultProps = {
-  type: 'default',
+  onRemoveMessage: PropTypes.func.isRequired,
+  message: PropTypes.shape({
+    text: PropTypes.string.isRequired,
+    type: PropTypes.oneOf(['default', 'success', 'danger']),
+    id: PropTypes.number.isRequired,
+    duration: PropTypes.number,
+  }).isRequired,
 };
