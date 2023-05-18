572a4b1156add3313562bb61d602fc3279d9a845
Caique Reis <caiquereiss@outlook.com>
2023-05-18 19:49:23 -0300

    message in modal

--- src/components/Toast/ToastMessage/styles.js
+++ src/components/Toast/ToastMessage/styles.js
@@ -1,5 +1,16 @@
-import styled, { css } from 'styled-components';
+import styled, { keyframes, css } from 'styled-components';
 
+const messageIn = keyframes`
+  from {
+    opacity: 0;
+    transform: translateY(100px);
+  }
+  to {
+    opacity: 1;
+    transform: translateY(0px);
+  }
+
+`;
 const containerVariants = {
   default: css`
   background: ${({ theme }) => theme.colors.primary.main};
@@ -20,6 +31,7 @@ export const Container = styled.div`
   align-items: center;
   justify-content: center;
   cursor: pointer;
+  animation: ${messageIn} 0.3s;
 
 
   ${({ type }) => containerVariants[type] || containerVariants.default}
